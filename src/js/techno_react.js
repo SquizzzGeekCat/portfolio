c3fbeb74f9f02330d8eebd72d23ed58a462f4637
SquizzzGeekCat <chomik.lu@gmail.com>
2024-10-09 09:10:51 +0200

    remplace the root assets

--- src/js/techno_react.js
+++ src/js/techno_react.js
@@ -6,87 +6,87 @@ function Technos() {
   return (
     <div className="grid gap-3 grid-cols-6 grid-rows-3 place-items-center w-full p-5 inner-techno">
       <Techno
-        road={"../assets/logo/html.png"}
+        road={"src/assets/logo/html.png"}
         desc={"Logo HTML"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/css.png"}
+        road={"src/assets/logo/css.png"}
         desc={"Logo CSS"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/js.png"}
+        road={"src/assets/logo/js.png"}
         desc={"Logo JS"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/tailwind.png"}
+        road={"src/assets/logo/tailwind.png"}
         desc={"Logo Tailwind framework CSS"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/react.png"}
+        road={"src/assets/logo/react.png"}
         desc={"Logo React JS"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/sql.png"}
+        road={"src/assets/logo/sql.png"}
         desc={"Logo SQL"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/mysql.png"}
+        road={"src/assets/logo/mysql.png"}
         desc={"Logo MySQL"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/oracle.png"}
+        road={"src/assets/logo/oracle.png"}
         desc={"Logo ORACLE"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/sqlServer.png"}
+        road={"src/assets/logo/sqlServer.png"}
         desc={"Logo SQL Server"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/php.png"}
+        road={"src/assets/logo/php.png"}
         desc={"Logo PHP"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/symfony.png"}
+        road={"src/assets/logo/symfony.png"}
         desc={"Logo Symfony framework PHP"}
         styleCss={"img_techno size_spe"}
       />
       <Techno
-        road={"../assets/logo/csharp.png"}
+        road={"src/assets/logo/csharp.png"}
         desc={"Logo C#"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/asp_net.png"}
+        road={"src/assets/logo/asp_net.png"}
         desc={"Logo ASP.Net Framwork C#"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/python.png"}
+        road={"src/assets/logo/python.png"}
         desc={"Logo Python"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/pandas.png"}
+        road={"src/assets/logo/pandas.png"}
         desc={"Logo Pandas framework Python"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/dart.png"}
+        road={"src/assets/logo/dart.png"}
         desc={"Logo Dart"}
         styleCss={"img_techno"}
       />
       <Techno
-        road={"../assets/logo/flutter.png"}
+        road={"src/assets/logo/flutter.png"}
         desc={"Logo Flutter"}
         styleCss={"img_techno"}
       />
